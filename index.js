36cf89962d9f3e717a4b75171c6e109690e0f9bb
hungryTechie <iirimina@schools.nyc.gov>
2024-08-01 20:43:05 -0400

    updated index.js

--- index.js
+++ index.js
@@ -6,6 +6,3 @@ container.innerHTML = "<button onclick='buy()'>Buy!</button>"
 // When clicked, render an HTML paragraph under the button (in the container)
 // that says "Thank you for buying!"
 
-// function buy(){
-//     container.innerHTML += "<p>Thank you for buying!</p>"
-// }
